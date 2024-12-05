868668957ff2c84df00f8f8c81f8887b02541e6e
Aashim <thelastmanarmy37@gmail.com>
2024-12-05 22:23:08 +0545

    NFT initialize

--- src/BasicNFT.sol
+++ src/BasicNFT.sol
@@ -3,5 +3,13 @@ pragma solidity >=0.8.0 <0.9.0;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNFT is ERC721 {
-    constructor() ERC721("Doggie", "DOG") {}
+    uint256 private s_tokenCounter;
+
+    constructor() ERC721("Nepali", "NEP") {
+        s_tokenCounter  = 0;
+    }
+    function mintNft() public {}
+    function tokenURI(uint256 tokenId) public view override returns(string memory) {
+        
+    }
 }
